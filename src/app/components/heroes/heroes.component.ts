7fed04145f61043b480e9ee194c5aa2a3a668e64
Alejandro Vivas <ajvivas86@gmail.com>
2019-06-03 11:53:39 -0400

    Heros Services

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
+import { HeroService } from '../../services/hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,16 +12,23 @@ import { HEROES } from './mock-heroes';
 export class HeroesComponent implements OnInit {
  
   //Variables
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
  
-  constructor() { }
+ 
+  constructor(private heroService: HeroService) { }
  
   ngOnInit() {
+    this.getHeroes();
   }
  
   //Methods
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
--- src/app/services/mock-heroes.ts
+++ src/app/services/mock-heroes.ts
@@ -1,4 +1,4 @@
-import { Hero } from './hero';
+import { Hero } from '../components/heroes/hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Nice' },
